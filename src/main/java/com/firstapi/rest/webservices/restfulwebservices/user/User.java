01b94ce65d7dc96fbb2ec748f7398e3e9f3b624b
nechaudhary <nechaudhary@paypal.com>
2022-04-15 21:11:32 +0530

    Validation message from the User to display in response

--- src/main/java/com/firstapi/rest/webservices/restfulwebservices/user/User.java
+++ src/main/java/com/firstapi/rest/webservices/restfulwebservices/user/User.java
@@ -6,7 +6,7 @@ import java.util.Date;
 
 public class User {
     private Integer id;
-    @Size(min=2) // constraint for validation that name should at least have 2 characters
+    @Size(min=2, message = "Name should have at least 2 characters") // constraint for validation that name should at least have 2 characters
     private String name;
     @Past
     private Date birthDate;
