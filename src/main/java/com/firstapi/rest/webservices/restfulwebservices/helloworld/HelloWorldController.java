9f92430b04c76eb1819431a63b3c438b8ed5aa0f
nechaudhary <nechaudhary@paypal.com>
2022-04-16 13:47:53 +0530

    Internationalisation initial

--- src/main/java/com/firstapi/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
+++ src/main/java/com/firstapi/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
@@ -1,27 +1,41 @@
 package com.firstapi.rest.webservices.restfulwebservices.helloworld;
 
 import com.firstapi.rest.webservices.restfulwebservices.helloworld.HelloWorldBean;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Locale;
 
 
 @RestController
 public class HelloWorldController {
+    @Autowired
+    private MessageSource messageSource;
+
     // GET method
     // URI - /hello-world
     // Map the GET URI to this controller
     // @RequestMapping(method= RequestMethod.GET, path="/hello-world")
-    @GetMapping(path="/hello-world")
-    public String helloWorld(){
+    @GetMapping(path = "/hello-world")
+    public String helloWorld() {
         return "Hello World";
     }
-    // return a bean
-    @GetMapping(path="/hello-world-bean")
-    public HelloWorldBean helloWorldBean(){
-        return new HelloWorldBean("Hello World")  ;
+
+    @GetMapping(path = "/hello-world-international")
+    public String helloWorld(@RequestHeader(name = "Accept-Language", required = false) Locale locale) {
+        return messageSource.getMessage("good.morning.message", null, locale);
     }
-    @GetMapping(path="/hello-world/{name}")
-    public HelloWorldBean helloWorldPathVariable(@PathVariable String name){
-        return new HelloWorldBean(String.format("Hello World, %s", name))  ;
+
+    // return a bean
+    @GetMapping(path = "/hello-world-bean")
+    public HelloWorldBean helloWorldBean() {
+        return new HelloWorldBean("Hello World");
+    }
+
+    @GetMapping(path = "/hello-world/{name}")
+    public HelloWorldBean helloWorldPathVariable(@PathVariable String name) {
+        return new HelloWorldBean(String.format("Hello World, %s", name));
     }
 
 }
